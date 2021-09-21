8476ffc71d5b1107701231dd7d03c2c3f102e238
se4en <001122mj@gmail.com>
2021-09-21 21:27:11 +0300

    Edit configs for multitask training

--- scripts/train_allennlp_local.sh
+++ scripts/train_allennlp_local.sh
@@ -2,17 +2,18 @@
 
 #
 # edit these variables before running script
-DATASET='ebmnlp'
-TASK='pico'
-with_finetuning='' #'_finetune'  # or '' for not fine tuning
-dataset_size=38124
+DATASET='citation_intent'
+TASK='text_classification'
+with_finetuning='_finetune' #'_finetune'  # or '' for not fine tuning
+with_multitask='_multitask' #'_multitask'  # or '' for not multitask
+dataset_size=1688
 
-export BERT_VOCAB=/net/nfs.corp/s2-research/scibert/scibert_scivocab_uncased/vocab.txt
-export BERT_WEIGHTS=/net/nfs.corp/s2-research/scibert/scibert_scivocab_uncased/weights.tar.gz
+export BERT_VOCAB=scibert_scivocab_uncased/vocab.txt
+export BERT_WEIGHTS=scibert_scivocab_uncased/weights.tar.gz
 
 export DATASET_SIZE=$dataset_size
 
-CONFIG_FILE=allennlp_config/"$TASK""$with_finetuning".json
+CONFIG_FILE=allennlp_config/"$TASK""$with_finetuning""$with_multitask".json
 
 SEED=13270
 PYTORCH_SEED=`expr $SEED / 10`
@@ -21,15 +22,17 @@ export SEED=$SEED
 export PYTORCH_SEED=$PYTORCH_SEED
 export NUMPY_SEED=$NUMPY_SEED
 
-export IS_LOWERCASE=false
+export IS_LOWERCASE=true
 export TRAIN_PATH=data/$TASK/$DATASET/train.txt
 export DEV_PATH=data/$TASK/$DATASET/dev.txt
 export TEST_PATH=data/$TASK/$DATASET/test.txt
+export AUX_PATH=data/$TASK/$DATASET/scaffolds/cite-sections.jsonl
+export AUX_2_PATH=data/$TASK/$DATASET/scaffolds/cite-worthiness.jsonl
 
-export CUDA_DEVICE=0
+export CUDA_DEVICE=-1
 
-export GRAD_ACCUM_BATCH_SIZE=32
-export NUM_EPOCHS=75
-export LEARNING_RATE=0.001
+export GRAD_ACCUM_BATCH_SIZE=16
+export NUM_EPOCHS=2
+export LEARNING_RATE=0.0001
 
-python -m allennlp.run train $CONFIG_FILE  --include-package scibert -s "$@"
+python scibert/training/train_local.py train_multitask_2 $CONFIG_FILE  --include-package scibert -s "$@"
